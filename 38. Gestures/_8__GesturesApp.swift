d90b55d9af37cca14f06763fb1c07268049e0392
SesiliTs <sesili.tsikaridze.1@btu.edu.ge>
2023-12-22 23:54:35 +0400

    Initial Commit

---
+++ 38. Gestures/_8__GesturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  _8__GesturesApp.swift
+//  38. Gestures
+//
+//  Created by Sesili Tsikaridze on 22.12.23.
+//
+
+import SwiftUI
+
+@main
+struct _8__GesturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
